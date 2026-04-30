16a44344a19b135be50115c9a0b53a7ceb4ab29d
Tyrrell Ruffin <tyruffin96@gmail.com>
2026-04-30 00:08:54 -0400

    Add AWS resources for VPC, subnets, and EC2 instance

--- main.tf
+++ main.tf
@@ -1,21 +1,84 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_instance" "github_actions_demo" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "GitHub-Actions-Demo"
+    Name = "devsecops-vpc"
+  }
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["71.69.55.11/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "web_server" {
+  ami           = "ami-098e39bafa7e7303d" # Amazon Linux
+  instance_type = "t3.micro"
+
+  key_name = "terraform-devops-key"
+
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "<h1>Deployed via Terraform + GitHub Actions</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "devsecops-ec2"
   }
 }
